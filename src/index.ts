69eb0ece965dd6fa165fc338d384fcbdefc85785
Каверза Артем <akaverza@phoenixit.ru>
2023-06-11 11:43:51 +0300

    2.4. Потоки RxJS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
+import { fromEvent } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 
-const data$ = ajax.getJSON('https://api.github.com/search/repositories?q=rxjs');
+const githubInput = document.querySelector('#githubInput');
+const gitlabInput = document.querySelector('#gitlabInput');
 
-data$.subscribe((value) => console.log('data$ value', value));
+const githubData$ = fromEvent(githubInput, 'input')
+    .pipe(
+        map((e) => (e.target as HTMLInputElement).value),
+        debounceTime(700),
+        distinctUntilChanged(),
+        switchMap((value) => ajax.getJSON(`https://api.github.com/search/repositories?q=${value}`))
+    );
 
-const dataGitLab$ = ajax.getJSON('https://gitlab.com/api/v4/projects?search=nodejs');
+githubData$.subscribe((value) => console.log('data$ value', value));
+
+const dataGitLab$ = fromEvent(gitlabInput, 'input')
+    .pipe(
+        map((e) => (e.target as HTMLInputElement).value),
+        debounceTime(700),
+        distinctUntilChanged(),
+        switchMap((value) => ajax.getJSON(`https://gitlab.com/api/v4/projects?search=${value}`))
+    );
 
 dataGitLab$.subscribe((value) => console.log('dataGitLab$ value', value));
